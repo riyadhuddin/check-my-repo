2f84edafb0990737f4c2c8ee45c4c1ba3d686a4e
discombobulateme <paloma.oliveira@saucelabs.com>
2020-12-28 12:39:02 +0100

    Add test for dashboard print func in index.js file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,6 +3,10 @@ const results = require('./__fixtures__/results')
 const resultsPassed = require('./__fixtures__/results-passed')
 const { createdInstances } = require('@octokit/rest')
 
+test('should return a sum of all repositories without fail', () => {
+  const log = jest.fn()
+  const hasFailures = 1
+  let passingRepositories = 0
 const fs = require('fs')
 jest.mock('fs')
 fs.promises = { mkdir: jest.fn(), writeFile: jest.fn() }
