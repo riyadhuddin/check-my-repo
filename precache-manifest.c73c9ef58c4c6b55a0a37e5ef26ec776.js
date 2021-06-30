76c9ce6ce5e77ac6e7673b1296057681d360ed8c
OSS Check My Repo <oss-check-my-repo-bot@users.noreply.github.com>
2021-06-30 00:14:44 +0000

    deploy: a1d15edd68e0869ee7231a993a0d1906f9274cc3

--- precache-manifest.c73c9ef58c4c6b55a0a37e5ef26ec776.js
+++ precache-manifest.c73c9ef58c4c6b55a0a37e5ef26ec776.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0ca55377aaae82768029",
+    "revision": "e5ced7fb67a06ac424ea",
     "url": "/check-my-repo/css/app.a81aad77.css"
   },
   {
@@ -28,12 +28,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/check-my-repo/img/x-circle.6d2ee7ef.svg"
   },
   {
-    "revision": "7a9ba3fbb1eb770d0984d3d5d2f3a7c2",
+    "revision": "74fad194835815b1530c8b45be105004",
     "url": "/check-my-repo/index.html"
   },
   {
-    "revision": "0ca55377aaae82768029",
-    "url": "/check-my-repo/js/app.4ee30340.js"
+    "revision": "e5ced7fb67a06ac424ea",
+    "url": "/check-my-repo/js/app.9d856932.js"
   },
   {
     "revision": "4f312830231cfb801796",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/check-my-repo/precache-manifest.500027c1235069b20899f7bf20db2983.js"
+  "/check-my-repo/precache-manifest.c73c9ef58c4c6b55a0a37e5ef26ec776.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
